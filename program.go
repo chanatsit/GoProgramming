a636e1cdb17fe35613ea84486a1674fed27d13c2
chanatsit <49265945+chanatsit@users.noreply.github.com>
2022-07-03 13:39:14 +0700

    Add files via upload

---
+++ program.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 45
+	//var name string = "Chanat"
+	name := "chanat"
+	fmt.Println("Hello Programming")
+	fmt.Println("Chanat  Sittipolpat")
+	fmt.Println("The age is ", age)
+	fmt.Println("name is ", name)
+	fmt.Printf("Type of age is %T", age)
+}
