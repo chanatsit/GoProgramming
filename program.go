7bc517f57e9f6f8ec9e1bb84487d2ee625199251
School Computer <chant.mathemetics@gmail.com>
2022-07-04 12:04:45 +0700

    Update Operation Version 12

--- program.go
+++ program.go
@@ -12,4 +12,13 @@ func main() {
 	fmt.Println("The age is ", age)
 	fmt.Println("name is ", name)
 	fmt.Printf("Type of age is %T", age)
+	// ตัวดำเนินการทางคณิตศาสตร์
+	// var number1 int = 10
+	// var number1, number2 = 10, 3
+	// number1 , number2 := 10, 3
+	number1 := 10
+	number2 := 3
+	fmt.Println("ผลบวก  = ", number1+number2)
+	fmt.Println("ผลหาร = ", number1/number2)
+	fmt.Println("เศษผลหาร = ", number1%number2)
 }
