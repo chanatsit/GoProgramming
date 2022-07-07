b2447aa1bf9dd212cbc720b45f231fc144299c53
chanat <chanat.mathematics@gmail.com>
2022-07-07 19:47:35 +0700

    Update Go Version16

--- program.go
+++ program.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	// เปลี่ยนแปลง
 	age := 80
+	//var name1 string
 	//var name string = "Chanat"
 	name := "chanat"
 	fmt.Println("Hello Programming")
@@ -18,6 +19,7 @@ func main() {
 	// number1 , number2 := 10, 3
 	number1 := 10
 	number2 := 3
+	var score int
 	fmt.Println("ผลบวก  = ", number1+number2)
 	fmt.Println("ผลหาร = ", number1/number2)
 	fmt.Println("เศษผลหาร = ", number1%number2)
@@ -26,4 +28,24 @@ func main() {
 	fmt.Println("จำนวนสองจำนวนนี้เท่ากันหรือไม่ ", number1 == number2)
 	fmt.Println("จำนวนที่หนึ่งมากกว่าจำนวนที่สองหรือไม่ ", number1 > number2)
 
+	//fmt.Print("ชื่อนักเรียน : ")
+	//fmt.Scanf("%s", &name1)
+	fmt.Print("คะแนนที่ได้รับ : ")
+	fmt.Scanf("%d", &score)
+	//fmt.Println("สวัสดี ", name1)
+	fmt.Println("คะแนนที่ได้ ", score)
+	if score >= 60 {
+		fmt.Println("คุณผ่านการทดสอบ")
+	} else {
+		fmt.Println("คุณไม่ผ่านการทดสอบ")
+	}
+	number3 := 0
+	switch number3 {
+	case 1:
+		fmt.Println("hello World")
+	case 2:
+		fmt.Println("Hello, Thailand")
+	default:
+		fmt.Println("ไม่ถูกต้อง")
+	}
 }
